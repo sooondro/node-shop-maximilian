013828450aac171d2dee8b3ecd117be615cfbeab
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-10-08 22:25:30 +0200

    Dynamic content using handlebars templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path')
+const rootDir = require("../util/path");
 
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
