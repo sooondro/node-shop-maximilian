69e770da2140a7227884cd42e42522371b4f67cb
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-10-08 21:52:33 +0200

    Dynamic content using pug templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,21 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path')
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
-router.post('/add-product', (req, res, next) => {
-    res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
