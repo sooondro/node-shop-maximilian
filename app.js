c60a3b0512c1f7318d349052bf24070a405b4fa4
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-10-10 05:29:39 +0200

    Templating and dynamic content with ejs

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", "views");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// ); //hbs is not supported out of the box
+
+// app.set("view engine", "hbs");
+// app.set("views", "views");
 
 // app.set('view engine', 'pug');
 // app.set('views', 'views');
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
